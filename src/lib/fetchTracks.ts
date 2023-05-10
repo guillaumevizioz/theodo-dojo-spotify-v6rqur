ec063d31380322424ea57cbf4bae0980c2135d79
guillaumevizioz <116821535+guillaumevizioz@users.noreply.github.com>
2023-05-10 20:19:44 +0200

    1 file modified

--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,4 +1,4 @@
-const apiToken: string = '';
+const apiToken: string = 'BQDXPq2tLPCnN4_3nzy5zpdiobi_VhzlnB4e5Y601BJ412aZ1WK7YaO7cfsghiG8GpjzpMIjNVjoRIUaxiPDdWmdTTojWhowMHff6BllVuvdaS0NaS8IvOQifmY0_ia6G8d71TiPK8ALP_QEk7ElZBr1AGINaI6v7cz4S8eWnc3W_r19R_Swr38k17jGMHJlLur40JoqOC1eZx3J-Pvdn0g';
 
 export const fetchTracks = async () => {
   const response = await fetch('https://api.spotify.com/v1/me/tracks', {
